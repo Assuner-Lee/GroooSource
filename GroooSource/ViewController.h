3003f498021d8e896030c983d16fdc6cf8ccb669
yonguang.li <yongguang.li@ele.me>
2017-02-06 13:24:28 +0800

    Initial Commit

---
+++ GroooSource/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GroooSource
+//
+//  Created by 李永光－饿了么－iOS开发 on 2017/2/6.
+//  Copyright © 2017年 Assuner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GroooSource/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GroooSource
+//
+//  Created by 李永光－饿了么－iOS开发 on 2017/2/6.
+//  Copyright © 2017年 Assuner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
